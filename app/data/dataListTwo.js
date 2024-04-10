814895c65a016a3842138b29458f1f82c47382ee
AJ <ajblankenship1@gmail.com>
2024-04-10 07:15:56 +0000

    added styling, description, and the ability to add your owns lists for comparison

---
+++ app/data/dataListTwo.js
@@ -0,0 +1,18 @@
+export default [
+    "Cherish",
+    "Gabriel",
+    "Zaira",
+    "Zaria",
+    "Felicity",
+    "Makaila",
+    "Gordon",
+    "Oscar",
+    "Brett",
+    "Sarahi",
+    "Margaret",
+    "Kaleb",
+    "Santino",
+    "Annaliese",
+    "Darrius",
+
+]
--- app/page.js
+++ app/page.js
@@ -1,95 +1,95 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import {useState} from 'react';
+import exampleDataListOne from "./data/dataListOne";
+import exampleDataListTwo from "./data/dataListTwo";
+
 
 export default function Home() {
+  const [makeRed,setMakeRed] = useState("");
+  const [makeGreen, setMakeGreen] = useState("");
+  const [inputListOne, setInputListOne] = useState('');
+  const [inputListTwo, setInputListTwo] = useState('');
+  const [listOneToDisplay, setListOneToDisplay] = useState(exampleDataListOne);
+  const [listTwoToDisplay, setListTwoToDisplay] = useState(exampleDataListTwo);
+
+
+
+function handleClick(){
+  setMakeGreen("makeGreen");
+  setMakeRed("makeRed");
+}
+
+function DisplayListOne(){
+  return listOneToDisplay.map((name, index)=>{
+      if(!listTwoToDisplay.includes(name)){
+        return <li key={`one-${index}`} className={makeRed}>{name}</li>
+      }else{
+        return <li key={`one-${index}`}>{name}</li>
+    }
+  });
+}
+
+function DisplayListTwo(){
+  return listTwoToDisplay.map((name, index)=>{
+      if(!listOneToDisplay.includes(name)){
+        return <li key={`two-${index}`} className={makeGreen}>{name}</li>;
+      }else{
+        return <li key={`two-${index}`} >{name}</li>;
+      }
+  })
+}
+
+function handleInputListOne(event){
+  setInputListOne(event.target.value);
+}
+function handleInputListTwo(event){
+  setInputListTwo(event.target.value);
+}
+
+function handleListOneSubmit(){
+  let newListOne = inputListOne.split(",");
+  setListOneToDisplay(newListOne);
+}
+
+function handleListTwoSubmit(){
+  let newListTwo = inputListTwo.split(",");
+  setListTwoToDisplay(newListTwo);
+}
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="arraysContainer">
+        <div className="arrayOne">
+          <h3>List One</h3>
+          <ul className="dataInListOne">
+             <DisplayListOne/>
+          </ul>
+        </div>
+        <div className='info-container'>
+          <h1>Compare Two Lists</h1>
+          <h3>Click the Compare button below to see how these two lists differ.</h3>
+          <p>The items List One that are not in List Two will turn red.</p>
+          <p> The itmes in List Two that are not in List One will turn green.</p>
+          <button onClick={handleClick} className='compare-button'>Compare</button>
+          <div className='your-own-lists-entry-container'>
+            <div className='your-own-list-one'>
+                <input className='list-input-field' type="text" placeholder='Enter items for List One here seperated by a comma.' value={inputListOne} onChange={handleInputListOne}/>
+                <button onClick={handleListOneSubmit}>Submit</button>
+            </div>
+            <div className='your-own-list-two'>
+              <input className='list-input-field' type="text"  placeholder='Enter items for List Two here seperated by a comma.' value={inputListTwo} onChange={handleInputListTwo}/>
+              <button onClick={handleListTwoSubmit}>Sumbit</button>
+            </div>
+          </div>
+        </div>
+        <div className="arrayTwo">
+          <h3>List Two</h3>
+          <ul className="dataInListTwo">
+            <DisplayListTwo/>
+          </ul>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   );
 }
